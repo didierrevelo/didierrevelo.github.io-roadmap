dc5ec23010e2ddf665d0516395d0e8a55dcfbf83
Didier Revelo <didierrevelo@gmail.com>
2025-08-25 00:21:13 +0000

    esta es la url https://github.com/didierrevelo/didierrevelo.github.io-ro

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
   },
   // Add the following configuration for GitHub Pages
   output: 'export',
-  // IMPORTANT: Replace 'cybersecurity-roadmap-navigator' with the name of your GitHub repository.
-  basePath: process.env.NODE_ENV === 'production' ? '/cybersecurity-roadmap-navigator' : '',
+  // IMPORTANT: Replace 'didierrevelo.github.io-roadmap' with the name of your GitHub repository.
+  basePath: process.env.NODE_ENV === 'production' ? '/didierrevelo.github.io-roadmap' : '',
   images: {
     unoptimized: true,
   },
