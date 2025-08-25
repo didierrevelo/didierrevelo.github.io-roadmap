f7a550007f89b3277ab01000ddcd4671ad5d591d
Didier Revelo <didierrevelo@gmail.com>
2025-08-25 01:37:05 +0000

    no funciona los estilos no se aplican y las expansiones no funcionan

--- next.config.js
+++ next.config.js
@@ -10,10 +10,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: 'export',
-  // Add the following configuration for GitHub Pages
-  // IMPORTANT: Replace 'roadmap' with the name of your GitHub repository if it's different.
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/roadmap' : undefined,
-  basePath: process.env.NODE_ENV === 'production' ? '/roadmap' : undefined,
   images: {
     unoptimized: true,
   },
